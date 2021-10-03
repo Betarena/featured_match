d6a4cbd5bc3419c60c31eeceb1900c2ce4a38a00
m <20924663+migbash@users.noreply.github.com>
2021-10-03 07:44:57 +0100

    updating main App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,24 @@
 
 <script lang="ts">
 	import Featured_Match_Widget from './components/_Featured_Match_Widget.svelte'
-
-	import { init_ApolloClient } from './functions/init_apollo'
+	import { fixtureVote } from './store/vote_fixture'
+	import { init_ApolloClient } from './graphql/init_apollo'
+	import { onMount } from 'svelte';
 
 	init_ApolloClient();
+
+	// ... kickstart the .localStorage();
+	onMount(async() => {
+		// Before we can use set on country we need to call the
+		// useLocalStorage doing this at the _layout file makes
+		// all routes and components exposed to this so we can
+		// import the state and call set anywhere like Btn.svelte
+		fixtureVote.useLocalStorage()
+		// Log it to make sure it works client side
+		// console.log(localStorage.getItem('cart'));
+		// solution to JSON Err,
+		// localStorage.clear();
+	})
 
 	// export let name: string
 </script>
